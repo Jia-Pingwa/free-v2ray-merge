6042cba238ed023ca204170260d9722a15eafbd6
Jia-Pingwa <lcfqc@protonmail.com>
2022-07-10 05:39:21 +0000

    重构代码+增加订阅

--- main.py
+++ main.py
@@ -3,23 +3,14 @@ import base64
 #读入webside.txt 中的内容，并按行分割，生成一个列表
 with open('webside.txt','r') as f:
     webside = f.read().splitlines()
-# 用request get列表中的每一个网址，并将结果写入到文件中
+# 用request get列表中的每一个网址，并将结果进行base64解码后加入列表txt
+txt = []
 for i in webside:
     r = requests.get(i)
-    # 对r进行base64解码
-    # print(r.text)
-    temp = base64.b64decode(r.text)
-    with open('output.txt','wb') as f:
-        f.write(temp)
-#读取output.txt中的内容
-with open('output.txt','r') as f:
-    txt = f.read()
-# 将output.txt中的内容按行分割，生成一个列表
-txt = txt.splitlines()
+    txt.append(base64.b64decode(r.text).decode('utf-8'))
 # 对txt列表进行去重
 txt = list(set(txt))
 # 将txt列表写入到文件中
 with open('output.txt','w') as f:
     for i in txt:
         f.write(i+'\n')
-
